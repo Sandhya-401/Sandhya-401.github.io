a5fa52a4f0a70fa42dfa4cadcd01e471d435ff72
Sandhya04 <[sandhya0401a3@gmail.com]>
2021-11-15 09:43:13 +0530

    Done with 1st version

---
+++ css-poc-2/index.js
@@ -0,0 +1,14 @@
+var accItem = document.getElementsByClassName('accordionitem');
+var accHeading = document.getElementsByClassName('accordionItemHeading');
+for (i = 0; i < accHeading.length; i++) {
+    accHeading[i].addEventListener('click', toggleItem);
+}
+function toggleItem() {
+    var itemClass = this.parentNode.className;
+    for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'accordionitem close';
+    }
+    if (itemClass == 'accordionitem close') {
+        this.parentNode.className = 'accordionitem open';
+    }
+}
